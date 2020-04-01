3a17eb6df864d11478aba10481319ac20d9dcfcd
morteify <michal.weglarz@protonmail.com>
2020-04-01 15:34:31 +0200

    Todos can be now added from AddTodo screen

--- src/screens/ToDoList/AddToDo.tsx
+++ src/screens/ToDoList/AddToDo.tsx
@@ -1,18 +1,53 @@
 import React, { useState } from "react";
-import { Text, View } from "react-native";
-import { Todo } from "../../reducers/todoReducer";
+import {
+  Text,
+  View,
+  TextInput,
+  Button,
+  Alert,
+  GestureResponderEvent
+} from "react-native";
+
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigation } from "@react-navigation/native";
 import styled from "styled-components/native";
+import { addTodo } from "../../actions";
 
 const Container = styled.View`
-  height: 50px;
+  margin-top: 100px;
   background-color: beige;
   border-bottom-width: 1px;
 `;
 
 export default function AddToDo() {
+  const dispatch = useDispatch();
+  const navigation = useNavigation();
+  const [currentTodo, setCurrentTodo] = useState<string | null>("");
+
+  const handleButtonPress = (event: GestureResponderEvent) => {
+    console.log("press");
+    dispatch(
+      addTodo({
+        id: Symbol(Date.now()),
+        date: Date.now(),
+        title: "Title",
+        description: currentTodo
+      })
+    );
+    setCurrentTodo("");
+    navigation.goBack();
+  };
+
   return (
     <Container>
       <Text>add to do</Text>
+      <Text>ToDo</Text>
+      <TextInput
+        style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
+        onChangeText={text => setCurrentTodo(text)}
+        value={currentTodo}
+      />
+      <Button title="Add todo" color="#841584" onPress={handleButtonPress} />
     </Container>
   );
 }
--- src/screens/ToDoList/ToDoListScreen.tsx
+++ src/screens/ToDoList/ToDoListScreen.tsx
@@ -9,7 +9,6 @@ import {
   View
 } from "react-native";
 import { useDispatch, useSelector } from "react-redux";
-import { addTodo } from "../../actions";
 import { TodoReducer, Todos, Todo } from "../../reducers/todoReducer";
 import ToDoListItem from "./ToDoListItem";
 import styled from "styled-components/native";
@@ -35,42 +34,14 @@ const Container = styled.View`
   flex: 1;
 `;
 
-const ToDoStack = createStackNavigator();
-
 function ToDoList() {
   const dispatch = useDispatch();
   const navigation = useNavigation();
-  const [currentTodo, setCurrentTodo] = useState<string | null>("");
   const todos: Array<Todo> = useSelector((state: TodoReducer) => state.todos);
-
-  const handleButtonPress = (event: GestureResponderEvent) => {
-    dispatch(
-      addTodo({
-        id: Symbol(Date.now()),
-        date: Date.now(),
-        title: "Title",
-        description: currentTodo
-      })
-    );
-    setCurrentTodo("");
-  };
 
   return (
     <Container>
       <ScrollView>
-        <Text>ToDo</Text>
-        <TextInput
-          style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
-          onChangeText={text => setCurrentTodo(text)}
-          value={currentTodo}
-        />
-        <Button
-          onPress={handleButtonPress}
-          title="Add todo"
-          color="#841584"
-          accessibilityLabel="Learn more about this purple button"
-        />
-
         <FlatList
           data={todos}
           renderItem={({ item }) => (
@@ -85,6 +56,7 @@ function ToDoList() {
     </Container>
   );
 }
+const ToDoStack = createStackNavigator();
 
 function ToDoListScreen() {
   return (
